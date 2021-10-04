2b06ea46677e24655b41ca4dac948c2a3d25f4da
alittek <43140594+alittek@users.noreply.github.com>
2021-10-04 16:38:42 +0100

    urls and views added for pages

--- theohealth/main/views.py
+++ theohealth/main/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+# index page
+def index(request):
+    context = {
+            "foo" : "bar",
+    }
+    return render(request, 'main/index.html', context)
+
+# login page
+def login(request):
+    context = {
+        "foo" : "bar",
+    }
+    return render(request, 'main/login.html', context)
+
+# overview all athletes page
+def overview(request):
+    context = {
+        "foo" : "bar",
+    }
+    return render(request, 'main/overview.html', context)
+
+# heatmap & info for one athlete
+def athlete(request):
+    context = {
+        "foo" : "bar",
+    }
+    return render(request, 'main/athlete.html', context)
+
+# page to add a new athlete
+def add_athlete(request):
+    context = {
+        "foo" : "bar",
+    }
+    return render(request, 'main/add_athlete.html', context)
+
+# instruct server to add new athlete
+def post_athlete(request):
+    context = {
+        "foo" : "bar",
+    }
+    return render(request, 'main/add_athlete.html', context)
