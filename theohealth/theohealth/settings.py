d08dc75d3faa9923fab1635b95d1ce048482de20
bkopplin <bjarne.kopplin@pm.me>
2021-10-04 14:47:05 +0100

    added skeleton for app main

--- theohealth/theohealth/settings.py
+++ theohealth/theohealth/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.PlaygroundConfig',
     'playground.apps.PlaygroundConfig',
     'django.contrib.admin',
     'django.contrib.auth',
