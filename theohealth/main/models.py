b7d65caa9ede2eef7dbb69a913f533aec0f18e89
bkopplin <bjarne.kopplin@pm.me>
2021-10-04 17:33:05 +0100

    show a list of all athletes

--- theohealth/main/models.py
+++ theohealth/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Therapist(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+class Athlete(models.Model):
+    therapist = models.ForeignKey(Therapist, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthdate = models.DateField()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
--- theohealth/main/views.py
+++ theohealth/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.views import generic
+
+from main.models import Athlete, Therapist
 
 # index page
 def index(request):
@@ -17,11 +19,15 @@ def login(request):
     return render(request, 'main/login.html', context)
 
 # overview all athletes page
-def overview(request, pk):
-    context = {
-        "foo" : "bar",
-    }
-    return render(request, 'main/overview.html', context)
+class OverviewView(generic.ListView):
+    template_name = 'main/overview.html'
+    context_object_name = 'athlete_list'
+
+    def get_queryset(self):
+        """
+        Return the list of athletes that are registered with the logged-in physiotherapist
+        """
+        return Athlete.objects.all()
 
 # heatmap & info for one athlete
 def athlete(request, pk):
