61e285874b38cbca1b2b7e5b1cc7195c2dc9ab79
MikolajOlejnik <molejnik@dundee.ac.uk>
2021-10-05 14:47:40 +0100

    __str__
    __str__ methods.

--- theohealth/main/models.py
+++ theohealth/main/models.py
@@ -20,8 +20,16 @@ class Workout(models.Model):
     date = models.DateField()
     athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE)
 
+    # Displays the name of the Athlete in the workout
+    def __str__(self):
+        return str(self.date) + " " + self.athlete.__str__()
+
+# Individual reading, each reading is connected to a workout id
 class SensorReading(models.Model):
     workout_num = models.ForeignKey(Workout, on_delete=models.CASCADE)
     sensor_num = models.IntegerField()
     timestamp = models.DateTimeField()
     heatvalue = models.IntegerField()
+
+    def __str__(self):
+        return str(self.sensor_num) + " " + str(self.timestamp)
