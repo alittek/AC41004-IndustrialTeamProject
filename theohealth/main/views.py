817923d60e107834166b2ddd4557ce2ec37d3fe6
MikolajOlejnik <molejnik@dundee.ac.uk>
2021-10-11 21:21:57 +0100

    Check for input params
    If statement to check whether there is a GET request.

--- theohealth/main/views.py
+++ theohealth/main/views.py
@@ -63,7 +63,13 @@ class OverviewView(generic.ListView):
         """
         Return the list of athletes that are registered with the logged-in physiotherapist
         """
-        return Athlete.objects.all()
+        search_param = self.request.GET.get("q")
+        # Filtering athletes
+        if search_param:
+            print("We got one: " + search_param)
+        else: # All Athletes
+            return Athlete.objects.all()
+        
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['user'] = self.request.user
