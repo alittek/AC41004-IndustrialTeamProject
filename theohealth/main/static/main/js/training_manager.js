10fdbbb067d0f8773263befd5f8cbfdcf37f7c13
bkopplin <bjarne.kopplin@pm.me>
2021-10-11 09:24:52 +0100

    changed timeout heatmap to 500ms

--- theohealth/main/static/main/js/training_manager.js
+++ theohealth/main/static/main/js/training_manager.js
@@ -114,7 +114,7 @@ function fetch_readings() {
     }
     update_heatmap([readings[0], readings[1], readings[2], readings[3]])
 
-    }, 100) // 1000s milliseconds
+    }, 500)
     }
   xHttp.open("GET", "/workout", true) // initialise request
   xHttp.send() // send request
