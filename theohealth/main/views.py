e17e1ba94db54e0cc1bcb36715e3ba11729008d3
bkopplin <bjarne.kopplin@pm.me>
2021-10-12 10:49:06 +0100

    added comment to get_context_data in OverviewView

--- theohealth/main/views.py
+++ theohealth/main/views.py
@@ -66,7 +66,11 @@ class OverviewView(generic.ListView):
         user = self.request.user
         if user.has_perm("auth.is_therapist"):
             return Athlete.objects.filter(therapist__auth_user=user).order_by('last_name')
+
     def get_context_data(self, **kwargs):
+        """
+        update the context data to include the user object
+        """
         context = super().get_context_data(**kwargs)
         context['user'] = self.request.user
         return context
