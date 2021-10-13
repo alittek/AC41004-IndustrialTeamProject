87cde303beb4f90c76af016f426c8551423a02ca
scottfulton2 <43143379+scottfulton2@users.noreply.github.com>
2021-10-13 11:56:12 +0100

    inner HTML for all sensors

--- theohealth/main/static/main/js/training_manager.js
+++ theohealth/main/static/main/js/training_manager.js
@@ -72,9 +72,9 @@ function fetch_readings() {
           max_readings[s] = readings[s]
           console.log("updated max readings: ", max_readings[0], max_readings[1], max_readings[2], max_readings[3])
           document.getElementById("highest-value-sen1").innerHTML = max_readings[0];
-          // document.getElementById("demo").innerHTML = 0;
-          // document.getElementById("demo").innerHTML = 0;
-          // document.getElementById("demo").innerHTML = 0;
+          document.getElementById("highest-value-sen2").innerHTML = max_readings[1];
+          document.getElementById("highest-value-sen3").innerHTML = max_readings[2];
+          document.getElementById("highest-value-sen4").innerHTML = max_readings[3];
       } else {
         clearInterval(simple_interval)
       }
