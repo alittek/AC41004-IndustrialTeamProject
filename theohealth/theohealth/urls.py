407c814ddc00dcba1cfcbc73b2ffd5addd852915
alittek <43140594+alittek@users.noreply.github.com>
2021-10-14 08:42:54 +0100

    remove playground files

--- theohealth/theohealth/urls.py
+++ theohealth/theohealth/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('main.urls')),
-    path('playground/', include('playground.urls')),
     path('admin/', admin.site.urls),
 ]
